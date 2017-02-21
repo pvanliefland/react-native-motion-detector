413d71c17db1a73e8057ce088599e7d0f24eb3c9
pvanliefland <pierre.vanliefland@gmail.com>
2017-02-21 13:43:12 +0100

    Added ios motion detector files

---
+++ ios/MotionDetector.h
@@ -0,0 +1,9 @@
+#import <React/RCTBridgeModule.h>
+#import <CoreMotion/CoreMotion.h>
+
+@interface MotionDetector : NSObject <RCTBridgeModule> {
+    CMMotionManager *_motionManager;
+}
+- (void) start;
+- (void) stop;
+@end
